eece4e8a719947b2426db40d519fff61046d6c9a
VulcanWM <logmedha09@gmail.com>
2023-04-18 21:07:38 +0100

    added syntax highlighting for the code

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/global.css';
+import 'highlight.js/styles/github.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/embed/[username]/[artwork].jsx
+++ pages/embed/[username]/[artwork].jsx
@@ -1,25 +1,15 @@
 import Layout from '../../../components/layout'
 import fsPromises from 'fs/promises';
 import path from 'path'
-import hljs from 'highlight.js/lib/core'
-import xml from 'highlight.js/lib/languages/xml'
-hljs.registerLanguage('xml', xml)
 
-export default function HomePage( {username, artworkCode, artwork}) {
-    const highlightedCode = hljs.highlight(artworkCode, { language: 'xml' }).value
-    console.log(highlightedCode)
+export default function HomePage( {artworkCode, artwork}) {
     return (
         <Layout pageTitle={artwork}>
-          <h2>{artwork}</h2>
-          <strong>By {username}</strong><br/>
-          <iframe width="400" height="300" scrolling="no" srcDoc={artworkCode} style={{border: "none"}} title={artwork}></iframe>
-          <pre>
-            <code dangerouslySetInnerHTML={{ __html: highlightedCode }} />
-            </pre>
+          <div dangerouslySetInnerHTML={{ __html: artworkCode }} />
         </Layout>
     );
 }
-  
+
   export async function getServerSideProps({ params }) {
     const username = params.username;
     const artwork = params.artwork
@@ -27,6 +17,7 @@ export default function HomePage( {username, artworkCode, artwork}) {
     var artworkCode;
     try{
        artworkCode = await (await fsPromises.readFile(filePath)).toString()
+       console.log(artworkCode)
     } catch {
         return {
             redirect: {
@@ -37,10 +28,8 @@ export default function HomePage( {username, artworkCode, artwork}) {
     }
     return {
       props: {
-        username,
         artworkCode,
-        artwork,
+        artwork
       },
     };
   }
-  
