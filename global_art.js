d108c6d912107fee543bf771a4a0b292f1de71a9
VulcanWM <logmedha09@gmail.com>
2023-04-18 21:26:39 +0100

    viewing art on home page

--- global_art.js
+++ global_art.js
@@ -1,4 +1,14 @@
 // this file contains an array with all the art ids that:
 // - can be displayed on the home page
 // - can be found via the search bar
-export const global_art = []
+export const global_art = [
+  'fredoist/ghost',
+  'ParanormalCoder/EmojiArt',
+  'ParanormalCoder/PerryThePlatypusArt',
+  'vulcanwm/cloud',
+  'vulcanwm/house',
+  'vulcanwm/mask',
+  'vulcanwm/soap',
+  'f4rukyldrm/terminal',
+  'manohar/sphare'
+]
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Layout from '../components/layout'
 import Link from 'next/link'
-// when getting the random art, rearrange the list and show 10 at a time
+import { global_art } from '../global_art';
+
 export default function HomePage() {
+    const random_art = global_art.sort((a, b) => 0.5 - Math.random());
     return (
         <Layout pageTitle="Home">
-          <h1>Hello</h1>
+          <h1>Global CSS Art</h1>
+          {random_art.map((artid, index) => (
+            <>
+             <Link href={'/art/' + artid}>{artid.split("/")[1]} by {artid.split("/")[0]}</Link><br/>
+             <iframe width="400" height="300" scrolling="no" style={{border: "none"}} src={"/embed/" + artid} title={artid}></iframe><br/>
+            </>
+          ))}
         </Layout>
     );
 }
