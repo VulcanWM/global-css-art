1ab935d64e30473af8fdd4c55c1abad6686fedef
VulcanWM <logmedha09@gmail.com>
2023-04-19 18:03:59 +0100

    made `random_art` a serversideprop in `index.js`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,11 @@ import Layout from '../components/layout'
 import Link from 'next/link'
 import { global_art } from '../global_art';
 
-export default function HomePage() {
-    const random_art = global_art.sort((a, b) => 0.5 - Math.random());
+export default function HomePage( {random_art}) {
     return (
         <Layout pageTitle="Home">
           <h1>Global CSS Art</h1>
-          {random_art.map((artid, index) => (
+          {random_art.map((artid) => (
             <>
              <Link href={'/art/' + artid}>{artid.split("/")[1]} by {artid.split("/")[0]}</Link><br/>
              <iframe width="400" height="300" scrolling="no" style={{border: "none"}} src={"/embed/" + artid} title={artid}></iframe><br/>
@@ -15,4 +14,13 @@ export default function HomePage() {
           ))}
         </Layout>
     );
+}
+
+export async function getServerSideProps({ params }) {
+  const random_art = global_art.sort((a, b) => 0.5 - Math.random());
+  return {
+    props: {
+      random_art
+    },
+  };
 }
